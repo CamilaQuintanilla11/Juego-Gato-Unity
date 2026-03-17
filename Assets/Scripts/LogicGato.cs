37ea24a1e3ee189790cad65881dab992e42a4c3a
Camila Quintanilla <camilaquintanillacabrera@gmail.com>
2026-03-16 21:13:34 -0600

    Update LogicGato.cs

--- Assets/Scripts/LogicGato.cs
+++ Assets/Scripts/LogicGato.cs
@@ -1,13 +1,35 @@
 using UnityEngine;
+using System.Collections.Generic;
 
 public class LogicGato : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public int[] secuencia = new int[9];
     void Start()
     {
-        
+        logicGato();
     }
+    void logicGato()
+    {
+        List<int> disponibles = new List<int>();
 
+        for (int i = 0; i < 9; i++)
+        {
+            disponibles.Add(i);
+        }
+        int turno = 0;
+
+        while (disponibles.Count > 0)
+        {
+            int mirandom = Random.Range(0, disponibles.Count);
+            int jugada = disponibles[mirandom];
+
+            secuencia[turno] = jugada;
+
+            disponibles.RemoveAt(mirandom);
+            turno++;
+        }
+    }
     // Update is called once per frame
     void Update()
     {
