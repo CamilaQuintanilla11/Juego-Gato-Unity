6be3b04448ac2705f72d35a4478f3b075d7ffb30
Camila Quintanilla <camilaquintanillacabrera@gmail.com>
2026-03-18 12:16:57 -0600

    combinación scripts (visual y base)

--- Assets/Scripts/VisualGato.cs
+++ Assets/Scripts/VisualGato.cs
@@ -5,6 +5,10 @@ using UnityEngine;
 public class VisualGato : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
+    [SerializeField] Material baseMat;
+    [SerializeField] Material cuadMat;
+    [SerializeField] PhysicsMaterial basePhysic;
+    [SerializeField] PhysicsMaterial cuadPhysic;
     public GameObject Circulo;
     public GameObject Cruz;
     public LogicGato logicg;
@@ -12,10 +16,51 @@ public class VisualGato : MonoBehaviour
     Vector3[] centros = new Vector3[9];
     void Start()
     {
+        MakePiso();
+        MakeCuadricula();
         guardarCentros();
         logicg.logicGato();
         visualGato();
     }
+
+    void MakePiso()
+    {
+        GameObject piso = GameObject.CreatePrimitive(PrimitiveType.Cube);
+        piso.transform.position = new Vector3(0, 0.125f, 0);
+        piso.transform.localScale = new Vector3(8,0.25f,8);
+
+        Rigidbody rb = piso.AddComponent<Rigidbody>();
+        rb.isKinematic = true;
+
+        MeshRenderer mr = piso.GetComponent<MeshRenderer>();
+        mr.material = baseMat;
+
+        Collider colp = piso.GetComponent<Collider>();
+        colp.material = basePhysic;
+    }
+    void MakeLineas(Vector3 pos, Vector3 lscale)
+    {
+        GameObject linea = GameObject.CreatePrimitive(PrimitiveType.Cube);
+        linea.transform.position = pos;
+        linea.transform.localScale = lscale;
+
+        Rigidbody rb = linea.AddComponent<Rigidbody>();
+        rb.isKinematic = true;
+
+        MeshRenderer mrl = linea.GetComponent<MeshRenderer>();
+        mrl.material = cuadMat;
+
+        Collider col = linea.GetComponent<Collider>();
+        col.material = cuadPhysic;
+
+    }
+    void MakeCuadricula()
+    {
+        MakeLineas(new Vector3(-1.5f, 0.25f, 0), new Vector3(0.2f, 0.25f, 8));
+        MakeLineas(new Vector3(1.5f, 0.25f, 0), new Vector3(0.2f, 0.25f, 8));
+        MakeLineas(new Vector3(0, 0.25f, -1.5f), new Vector3(8, 0.25f, 0.2f));
+        MakeLineas(new Vector3(0, 0.25f, 1.5f), new Vector3(8, 0.25f, 0.2f));
+   }
     void visualGato()
     {
         Debug.Log("VisualGato funcionando");
